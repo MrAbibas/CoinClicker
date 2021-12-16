2a269e264ef11d4cbfd7a8af9c1c727c3f7f1ff9
MrAbibas <73127648+MrAbibas@users.noreply.github.com>
2021-12-16 10:22:56 +0200

    Add coin counter

---
+++ Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CoinCounter : MonoBehaviour
+{
+    public UnityEvent<int> onCoinCountChanged;
+    [SerializeField]
+    private int _startCoinCount;
+    private int _cointCount;
+    private int coinCount
+    {
+        get => _cointCount;
+        set
+        {
+            _cointCount = value;
+            onCoinCountChanged?.Invoke(value);
+        }
+    }
+    private void Start()
+    {
+        _cointCount = _startCoinCount;
+        FindObjectOfType<CoinSpawner>().onCoinSpawned.AddListener(OnCoinSpawned);
+    }
+    private void OnCoinSpawned()
+    {
+        _cointCount--;
+        if (_cointCount == 0)
+            _cointCount = _startCoinCount;
+    }
+}
