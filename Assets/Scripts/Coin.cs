917493d43eff8841dbe9536db87307752fed4941
MrAbibas <73127648+MrAbibas@users.noreply.github.com>
2021-12-16 08:59:55 +0200

    Add coins pull
    Добавил пул монет

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Coin : MonoBehaviour
+{
+    public UnityEvent<Coin> onChestCollision;
+    private Rigidbody _rigidbody;
+    private Vector3 _startPosition;
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _startPosition = transform.position;
+    }
+    public void ToStartPosition()
+    {
+        _rigidbody.isKinematic = true;
+        transform.position = _startPosition;
+    }
+}
---
+++ Assets/Scripts/CoinsPool.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsPool : MonoBehaviour
+{
+    private Stack<Coin> coins;
+    [SerializeField]
+    private int _startSize;
+    [SerializeField]
+    private Coin _coinPrefab;
+    public int Size => coins.Count;
+    private void Start()
+    {
+        coins = new Stack<Coin>();
+        for (int i = 0; i < _startSize; i++)
+            AddCoinToStack();
+    }
+    public void Push(Coin coin)
+    {
+        coin.ToStartPosition();
+        coins.Push(coin);
+    }
+    public Coin Pop()
+    {
+        if (coins.Count == 0)
+            AddCoinToStack();
+        return coins.Pop();
+    }
+    private void AddCoinToStack()
+    {
+        Coin newCoin = Instantiate(_coinPrefab, transform.position, Quaternion.identity);
+        coins.Push(newCoin);
+    }
+}
